e1e1b3fef221c6feb3c86790f4e1a06a7f1a7053
nkt929 <frost339@list.ru>
2021-09-28 01:25:13 +0500

    lab12

--- app_python/main.py
+++ app_python/main.py
@@ -14,7 +14,23 @@ def get_time():
     formats time to the template and returns the resulting string.
     :return: {string} current time
     """
+    count = 0
+    with open("./volumes/count", "r") as f:
+        count = int(f.read()) + 1
+    with open("./volumes/count", "w") as f:
+        f.write(str(count))
+
     tz_moscow = pytz.timezone('Europe/Moscow')
     datetime_moscow = datetime.datetime.now(tz_moscow)
     server_time = datetime_moscow.strftime('%A %B, %d %Y %H:%M:%S')
     return "server time {}".format(server_time)
+
+@app.route("/visits", methods=['GET'])
+def get_time():
+    """
+    Shows the visits' counter
+    """
+    count = 0
+    with open("./volumes/count", "r") as f:
+        count = int(f.read())
+    return "visits {}".format(count)
